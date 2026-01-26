0e3ca9429da22d5af847ed5c0678006eb3a5c69b
Diya Jain <diyajain.199005@gmail.com>
2026-01-26 18:36:14 +0530

    Creating basic tkinter gui window

---
+++ pdf_chatbot_gui.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("RAG PDF Chatbot")
+root.geometry("700x600")
+
+title = tk.Label(root, text="RAG PDF Chatbot", font=("Arial", 16))
+title.pack(pady=10)
+
+root.mainloop()
