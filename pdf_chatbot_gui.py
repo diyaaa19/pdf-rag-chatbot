f3bbc9ed9c5d8b7f104bf2f6046e1d12881522e4
Diya Jain <diyajain.199005@gmail.com>
2026-01-28 04:10:40 +0530

    Adding drag and drop pdf input feature

--- pdf_chatbot_gui.py
+++ pdf_chatbot_gui.py
@@ -1,10 +1,22 @@
 import tkinter as tk
+from tkinterdnd2 import TkinterDnD, DND_FILES
 
-root = tk.Tk()
+root = TkinterDnD.Tk()
 root.title("RAG PDF Chatbot")
 root.geometry("700x600")
 
 title = tk.Label(root, text="RAG PDF Chatbot", font=("Arial", 16))
 title.pack(pady=10)
+
+pdf_entry = tk.Entry(root, width=70)
+pdf_entry.pack(pady=10)
+
+def drop(event):
+    file_path = event.data.strip("{}")
+    pdf_entry.delete(0, tk.END)
+    pdf_entry.insert(0, file_path)
+
+pdf_entry.drop_target_register(DND_FILES)
+pdf_entry.dnd_bind("<<Drop>>", drop)
 
 root.mainloop()
